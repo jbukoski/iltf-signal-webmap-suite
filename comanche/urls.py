2a8653daf88a47340b64b78a9dfb7535b6198ba4
jbukoski <jacob.bukoski@yale.edu>
2017-02-15 11:16:18 -0800

    updated files

--- comanche/urls.py
+++ comanche/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^counties/', GeoJSONLayerView.as_view(model=counties), name='counties'),
     url(r'^cotton/', GeoJSONLayerView.as_view(model=cotton), name='cotton'),
+    url(r'^caddo/', GeoJSONLayerView.as_view(model=caddo), name='caddo'),
 ]
