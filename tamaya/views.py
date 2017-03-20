b7c73bbbd7e9716f6f53a38b9738960af42cf126
jbukoski <jacob.bukoski@yale.edu>
2017-03-20 16:43:02 -0700

    updated files

--- tamaya/views.py
+++ tamaya/views.py
@@ -42,22 +42,76 @@ def user_polygons_view(request):
     user_polygons_json = serialize('geojson', user_polygons.objects.all(), geometry_field="geom", fields=('name', 'comment'))
     return HttpResponse(user_polygons_json, content_type='json')
 
-# Downloads
+###############
+## Downloads ##
+###############
+
+# Specify downloads path
+path = os.path.dirname(os.path.abspath(__file__))
+
+# Admin layers
+
+def boundary_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'boundary.zip'), 'rb')
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="boundary.zip"'
+
+    return response
+
+def mbl_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'mbl_int.zip'), 'rb')
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="mbl_int.zip"'
+
+    return response
+
+def roads_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'santa_ana_roads.zip'), 'rb')
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="reservation_roads.zip"'
+
+    return response
+
+# Soi layers
 
 def bd_dl_view(request):
-    path = os.path.dirname(os.path.abspath(__file__))
-
     download_file = open(os.path.join(path, 'downloads', 'bulk_density_1_3_bar.zip'), "rb")
     response = HttpResponse(download_file, content_type='application/force-download')
     response['Content-Disposition'] = 'attachment; filename="bulk_density_1_3_bar.zip"'
 
     return response
 
-def ph_dl_view(request):
-    path = os.path.dirname(os.path.abspath(__file__))
+def compost_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'composting_medium_and_final_cover.zip'), "rb")
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="composting_medium_and_final_cover.zip"'
 
+    return response
+
+def om_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'organic_matter.zip'), "rb")
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="organic_matter.zip"'
+
+    return response
+
+def ph_dl_view(request):
     download_file = open(os.path.join(path, 'downloads', 'ph_surface_weighted_average.zip'), "rb")
     response = HttpResponse(download_file, content_type='application/force-download')
     response['Content-Disposition'] = 'attachment; filename="ph_surface_weighted_average.zip"'
+
+    return response
+
+def taxonomy_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'soil_taxonomy.zip'), "rb")
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="soil_taxonomy.zip"'
+
+    return response
+
+def texture_dl_view(request):
+    download_file = open(os.path.join(path, 'downloads', 'soil_texture_dcp.zip'), "rb")
+    response = HttpResponse(download_file, content_type='application/force-download')
+    response['Content-Disposition'] = 'attachment; filename="soil_texture_dcp.zip"'
 
     return response
